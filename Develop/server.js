160e5b27eec7e4c050a3e8a626f8b50ae7855c0e
FCTESLA <fcamacho888@gmail.com>
2020-04-24 12:27:10 -0700

    HTML routes

--- Develop/server.js
+++ Develop/server.js
@@ -8,3 +8,17 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"))
+
+//--HTML-Routes--//
+
+app.get("/notes", (req, res)=>{
+    res.sendfile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.get("*", (req, res)=>{
+    res.sendfile(path.join(__dirname, "/public/index.html"));
+});
+
+app.listen(PORT, function(){
+    console.log("App listening on PORT " + PORT);
+})
