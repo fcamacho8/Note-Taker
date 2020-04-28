aeac028437a17573ce10a428480733d67eb5eaff
FCTESLA <fcamacho888@gmail.com>
2020-04-27 22:52:58 -0700

    GET /api/notes reading db.json

--- Develop/server.js
+++ Develop/server.js
@@ -11,14 +11,24 @@ app.use(express.static("public"))
 
 //--HTML-Routes--//
 
-app.get("/notes", (req, res)=>{
+app.get("/notes", (req, res) => {
     res.sendfile(path.join(__dirname, "/public/notes.html"));
 });
 
-app.get("*", (req, res)=>{
+app.get("*", (req, res) => {
     res.sendfile(path.join(__dirname, "/public/index.html"));
 });
 
-app.listen(PORT, function(){
+app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
+    
 })
+
+//--API Routes--//
+
+app.get("/api/notes", function (req, res) {
+    fs.readFile(path.join(__dirname, "/db/db.json"), "utf-8", (err, data) => {
+        if (err) throw err;
+        return res.json(data);
+    });
+});
